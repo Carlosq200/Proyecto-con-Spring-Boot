3dcd6efd66bd62e0c472ee6716be5795580f0f14
Carlos Quino <cquinoneza@miumg.edu.gt>
2025-10-10 21:04:43 -0600

    test: añadir prueba unitaria simple para validar saludo

---
+++ src/test/java/com/example/proyecto/web/HelloControllerTest.java
@@ -0,0 +1,13 @@
+﻿package com.example.proyecto.web;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+    @Test
+    void saludoEmpiezaConHola() {
+        String msg = "Hola desde Spring Boot";
+        assertTrue(msg.startsWith("Hola"));
+    }
+}
